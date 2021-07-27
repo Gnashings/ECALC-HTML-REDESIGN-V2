0c331e00bd2123e50aefdd6327eae20db3aa66cb
Nash Roy <nashroy11@gmail.com>
2021-07-27 16:36:30 -0700

    Update VideoPlayer.js

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -1,31 +1,14 @@
 var vid = document.getElementById("VideoPlayer");
 var isPlaying;
 
-vid.onplaying = function() {
-    isPlaying = true;
-}
-
-vid.onpause = function () {
-    isPlaying = false;
-}
-
-function playVid() { 
-  vid.play(); 
-} 
-
-function pauseVid() { 
-  vid.pause();
-} 
-
 function PlayPauseSwap() {
-    if (isPlaying = true)
-    {
-        pauseVid();
-        console.log(isPlaying);
-    }
-    else if (isPlaying = false)
-    {
-        playVid();
-        console.log(isPlaying);
-    }
-}
+    isPlaying ? vid.pause() : vid.play();
+  };
+  
+  vid.onplaying = function() {
+    isPlaying = true;
+  };
+
+  vid.onpause = function() {
+    isPlaying = false;
+  };
